36ef246977de2aaf769d9c1ad1119bc8c456d462
burlap101 <josephcrowley85@gmail.com>
2024-11-11 18:49:51 +0800

    attempted adding sparql ls but not quite working

--- lua/joecrowley/lsp/config.lua
+++ lua/joecrowley/lsp/config.lua
@@ -3,14 +3,15 @@ local lspconfig = require('lspconfig')
 local vim = vim
 
 lspconfig.gopls.setup {
-    cmd = { "gopls", "serve" },
+	cmd = { "gopls", "serve" },
 }
 lspconfig.hls.setup {
-    filetypes = { "haskell", "lhaskell", "cabal", "hs" },
-    settings = {
-        formattingProvider = "fourmolu"
-    },
+	filetypes = { "haskell", "lhaskell", "cabal", "hs" },
+	settings = {
+		formattingProvider = "fourmolu"
+	},
 }
+--lspconfig.sparql_ls.setup {}
 -- lspconfig.denols.setup {
 -- 	root_dir = vim.fs.dirname(vim.fs.find({ 'deno.json', 'deno.jsonc' }, { upward = true })[1]),
 -- 	init_options = {
@@ -18,95 +19,94 @@ lspconfig.hls.setup {
 -- 	},
 -- }
 lspconfig.pyright.setup {
-    settings = {
-        pyright = {
-            disableOrganizeImports = true,
+	settings = {
+		pyright = {
+			disableOrganizeImports = true,
 			openFilesOnly = true,
-        },
-        python = {
+		},
+		python = {
 			analysis = {
 				autoImportCompletions = false,
 				diagnosticMode = "openFilesOnly",
-				typeCheckingMode = "standard",
+				typeCheckingMode = "off",
 				diagnosticSeverityOverrides = {
-					reportAttributeAccessIssue="warning",
+					reportAttributeAccessIssue = "warning",
 				},
 			}
 		},
-    },
+	},
+	rootMarkers = { ".git", "pyproject.toml" },
 }
 
--- lspconfig.efm.setup {
---     init_options = { documentFormatting = false },
---     settings = {
---         rootMarkers = { ".venv/", ".git/" },
---         languages = {
---             python = {
---                 {
---                     lintCommand = "mypy --show-column-numbers",
---                     lintFormats = {
---                         "%f:%l:%c: %trror: %m",
---                         "%f:%l:%c: %tarning: %m",
---                         "%f:%l:%c: %tote: %m",
---                     },
--- 					lintOnSave = true,
--- 					lintSource = "mypy",
---                 },
---             }
---         }
---     }
--- }
-lspconfig.jsonls.setup {}
-lspconfig.svelte.setup {}
-lspconfig.lua_ls.setup {
-    on_init = function(client)
-        if client.workspace_folders then
-            local path = client.workspace_folders[1].name
-            if vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc') then
-                return
-            end
-        end
-		-- Check to see if working on neovim conf
-		if not string.match(vim.uv.cwd() or "", "/home/.*%.config/nvim") then
-			client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-				runtime = {
-					version = 'Lua 5.4',
-					path = {
-						'?.lua',
-						'?/init.lua',
-						vim.fn.expand'~/.luarocks/share/lua/5.4/?.lua',
-						vim.fn.expand'~/.luarocks/share/lua/5.4/?/init.lua',
-						'/usr/share/5.4/?.lua',
-						'/usr/share/5.4/?/init.lua'
-					},
-				},
-				workspace = {
-					library = {
-						vim.fn.expand'~/.luarocks/share/lua/5.4',
-						'/usr/share/lua/5.4'
-					}
-				}
-			})
-		end
-    end,
+lspconfig.efm.setup {
+    init_options = { documentFormatting = false },
     settings = {
-        Lua = {
-            diagnostics = {
-                disable = { "missing-fields" },
+        rootMarkers = { ".venv/", ".git/" },
+        languages = {
+            python = {
+                {
+                    lintCommand = "mypy --show-column-numbers",
+                    lintFormats = {
+                        "%f:%l:%c: %trror: %m",
+                        "%f:%l:%c: %tarning: %m",
+                        "%f:%l:%c: %tote: %m",
+                    },
+					lintOnSave = true,
+					lintSource = "mypy",
+                },
             }
         }
     }
+}
+lspconfig.jsonls.setup {}
+lspconfig.svelte.setup {}
+lspconfig.lua_ls.setup {
+	on_init = function(client)
+		if client.workspace_folders then
+			local path = client.workspace_folders[1].name
+			if vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc') then
+				return
+			end
+		end
+
+		client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+			runtime = {
+				-- Tell the language server which version of Lua you're using
+				-- (most likely LuaJIT in the case of Neovim)
+				version = 'LuaJIT'
+			},
+			-- Make the server aware of Neovim runtime files
+			workspace = {
+				checkThirdParty = false,
+				library = {
+					vim.env.VIMRUNTIME
+					-- Depending on the usage, you might want to add additional paths here.
+					-- "${3rd}/luv/library"
+					-- "${3rd}/busted/library",
+				}
+				-- or pull in all of 'runtimepath'. NOTE: this is a lot slower and will cause issues when working on your own configuration (see https://github.com/neovim/nvim-lspconfig/issues/3189)
+				-- library = vim.api.nvim_get_runtime_file("", true)
+			}
+		})
+	end,
+	settings = {
+		Lua = {
+			diagnostics = {
+				disable = { "missing-fields" },
+			}
+		}
+	}
 }
 lspconfig.tailwindcss.setup {}
 lspconfig.volar.setup {}
 --lspconfig.tsserver.setup {}
 lspconfig.ruff.setup {
-    init_options = {
-        settings = {
-            configuration = "pyproject.toml",
-            configurationPreferences = "filesystemFirst",
-        }
-    }
+	init_options = {
+		settings = {
+			configuration = "pyproject.toml",
+			configurationPreferences = "filesystemFirst",
+		}
+	}
 }
 
 -- SVELTE GLOBAL
@@ -137,25 +137,25 @@ vim.api.nvim_create_autocmd('LspAttach', {
 			end
 		end
 
-        -- Buffer local mappings.
-        -- See `:help vim.lsp.*` for documentation on any of the below functions
-        local opts = { buffer = ev.buf }
-        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-        vim.keymap.set({'n', 'i', 'c'}, '<C-k>', vim.lsp.buf.signature_help, opts)
-        vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
-        vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
-        vim.keymap.set('n', '<leader>wl', function()
-            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-        end, opts)
-        vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
-        vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-        vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
-        vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-        vim.keymap.set('n', '<leader>ff', function()
-            vim.lsp.buf.format({ async = true })
-        end, opts)
-    end
+		-- Buffer local mappings.
+		-- See `:help vim.lsp.*` for documentation on any of the below functions
+		local opts = { buffer = ev.buf }
+		vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+		vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+		vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+		vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+		vim.keymap.set({ 'n', 'i', 'c' }, '<C-k>', vim.lsp.buf.signature_help, opts)
+		vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
+		vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
+		vim.keymap.set('n', '<leader>wl', function()
+			print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+		end, opts)
+		vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
+		vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+		vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+		vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+		vim.keymap.set('n', '<leader>ff', function()
+			vim.lsp.buf.format({ async = true })
+		end, opts)
+	end
 })
--- lua/joecrowley/lsp/init.lua
+++ lua/joecrowley/lsp/init.lua
@@ -1,3 +1,4 @@
 require("nvim-lsp-installer").setup {}
+require("joecrowley.lsp.sparql-ls")
 require("joecrowley.lsp.config")
 -- require("joecrowley.lsp.metals")
