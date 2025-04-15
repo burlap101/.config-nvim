c9f56ae0a3dcd8a35558ff0ddf1fff05ef168e43
burlap101 <josephcrowley85@gmail.com>
2025-04-15 08:33:16 +0800

    converted biome efm format command to utilise the npm package installed within a repo.

--- lua/joecrowley/lsp/configs/efm_ls.lua
+++ lua/joecrowley/lsp/configs/efm_ls.lua
@@ -1,16 +1,27 @@
 local lspconfig = require("lspconfig")
+local fs = require("efmls-configs.fs")
 
 -- EFM lang server using efmls configs
 local function generate_efm_config()
 	local eslint = require('efmls-configs.linters.eslint')
 	local prettier = require('efmls-configs.formatters.prettier')
 	local biome_f = require('efmls-configs.formatters.biome')
+	local myBiomeCmd = function()
+		local formatter = 'npx @biomejs/biome'
+		local args = "check --apply --stdin-file-path '${INPUT}'"
+		return string.format('%s %s', fs.executable(formatter, fs.Scope.NODE), args)
+	end
+	-- Added for synergy
+	biome_f = vim.tbl_deep_extend('force', biome_f, {
+		formatCommand = myBiomeCmd(),
+	})
 	local mypy = require('efmls-configs.linters.mypy')
 	local languages = {
 		typescript = { eslint, prettier, biome_f },
 		javascript = { eslint, prettier, biome_f },
 		python = { mypy },
 	}
+
 
 	return {
 		filetypes = vim.tbl_keys(languages),
