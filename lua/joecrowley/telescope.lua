6de37bc9cf5c8d1c637e0c5ecaae6e82e210cc30
burlap101 <josephcrowley85@gmail.com>
2025-04-17 20:42:36 +0800

    added a keybind to allow transferring picker results to a buffer for further editing

--- lua/joecrowley/telescope.lua
+++ lua/joecrowley/telescope.lua
@@ -1,4 +1,6 @@
 local telescope = require("telescope")
+local actions_state = require("telescope.actions.state")
+local actions = require("telescope.actions")
 
 telescope.setup {
 	extensions = {
@@ -8,6 +10,29 @@ telescope.setup {
 			layout_config = {
 				preview_height = 0.8,
 			},
+		}
+	},
+	defaults = {
+		mappings = {
+			i = {
+				["<C-e>"] = function(prompt_bufnr)
+					local picker = actions_state.get_current_picker(prompt_bufnr)
+					local results = picker.finder.results
+
+					local lines = {}
+					for _, entry in ipairs(results) do
+						table.insert(lines, entry.text)
+					end
+					print(vim.print(lines))
+
+					-- Open a new buffer for editing
+					vim.api.nvim_command("new")
+					vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
+
+					-- Close Telescope after processing
+					actions.close(prompt_bufnr)
+				end
+			}
 		}
 	}
 }
