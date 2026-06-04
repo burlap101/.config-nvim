9cc60e9fa215bc8db1b0ad4a878070cd518a9b5f
burlap101 <josephcrowley85@gmail.com>
2026-06-04 11:52:15 +0800

    feat: python ty, postgres ls and buf_ls added

--- lua/joecrowley/lsp/configs/protobuf.lua
+++ lua/joecrowley/lsp/configs/protobuf.lua
@@ -1,3 +1,15 @@
-vim.lsp.config("buf_ls", {})
+---@return [string]
+local function root_marker()
+    if vim.fs.find("buf.yaml", {})[1] then
+        return { 'buf.yaml' }
+    end
+    return { '.git/' }
+end
+
+vim.lsp.config('buf_ls', {
+    cmd = { 'buf', 'lsp', 'serve' },
+    filetypes = { 'proto', 'buf-config' },
+    root_markers = root_marker(),
+})
 
 vim.lsp.enable("buf_ls", true)
--- lua/joecrowley/lsp/configs/python.lua
+++ lua/joecrowley/lsp/configs/python.lua
@@ -25,7 +25,22 @@ vim.lsp.config("ruff", {
     rootMarkers = { ".git/", "pyproject.toml", ".venv/" },
 })
 
-vim.lsp.enable("pyright", true)
+vim.lsp.config("ty", {
+    rootMarkers = { ".git/", "pyproject.toml", ".venv/" },
+})
+
 vim.lsp.enable("ruff", true)
 
+local function has_ty()
+    return vim.fn.executable("ty") == 1
+end
+
+-- If ty is installed favour it over pyright.
+if has_ty() then
+    vim.lsp.enable("ty", true)
+    vim.lsp.enable("pyright", false)
+else
+    vim.lsp.enable("pyright", true)
+    vim.lsp.enable("ty", false)
+end
 
